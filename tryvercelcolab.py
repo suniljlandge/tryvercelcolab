f7df0e5486efac8fcf562a798f58c93e27a7839a
suniljlandge <117426980+suniljlandge@users.noreply.github.com>
2023-01-26 15:21:24 +0530

    Update tryvercelcolab.py

--- tryvercelcolab.py
+++ tryvercelcolab.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://drive.google.com/file/d/<colab_notebook_id>', 'colab.ipynb', quiet=False)
+    gdown.download('https://drive.google.com/file/d/1yzzW0k4vyrYrisrRfvXxLCdZOBLLAMI-', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
